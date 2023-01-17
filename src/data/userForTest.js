1503b508fb92eb912441bcbe758930f7a5172556
NicolasCiclet <ciclet.nicolas@free.fr>
2023-01-17 15:51:39 +0100

    Add Redux, Store, Reducer

---
+++ src/data/userForTest.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    firstname: 'Sylvie',
+    lastname: 'Syl',
+    gender: 'Female',
+    city: 'Paris',
+  },
+  {
+    firstname: 'Jean',
+    lastname: 'Dupont',
+    gender: 'male',
+    city: 'Cannes',
+  },
+  {
+    firstname: 'Helene',
+    lastname: 'Rollen',
+    gender: 'Female',
+    city: 'Lyon',
+  },
+  {
+    firstname: 'Alain',
+    lastname: 'Pierre',
+    gender: 'male',
+    city: 'Limoges',
+  },
+];
---
+++ src/reducers/userReducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+  // ici le state initial
+};
+
+const userReducer = (state = initialState, action = {}) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default userReducer;
