78b258c6a1bdac95f04a797303bc07d549259dd1
MarineBourriquen <marinebourriquen@hotmail.com>
2023-01-24 16:35:22 +0100

    tentative de css

--- src/components/Profil/index.js
+++ src/components/Profil/index.js
@@ -18,68 +18,68 @@ const Profil = () => {
       {/* menu avec des ancres menant plus bas dans la page,
       à mettre en menu burger pour le mobil */}
       <div className="profil-nav">
-        <HashLink smooth to="/Profil#mon-profil">
+        <HashLink className="profil-link" smooth to="/Profil#mon-profil">
           Mon profil
         </HashLink>
-        <HashLink smooth to="/Profil#mes-animaux">
+        <HashLink className="profil-link" smooth to="/Profil#mes-animaux">
           Mes animaux
         </HashLink>
-        <HashLink smooth to="/Profil#mes-messages">
+        <HashLink className="profil-link" smooth to="/Profil#mes-messages">
           Mes messages
         </HashLink>
-        <HashLink smooth to="/Profil#mes-événements">
+        <HashLink className="profil-link" smooth to="/Profil#mes-événements">
           Mes événements
         </HashLink>
       </div>
-      <div id="mon-profil">
+      <div className="profil-section" id="mon-profil">
         {/* Afficher les infos du user connecté */}
         <h1 className="profil-title">Bonjour {user.firstname} {user.lastname}</h1>
         <img className="profil-photo" src={user.userPicture} alt="user" />
-        <h3 className="profil-info">Nickname:</h3>
+        <h3 className="profil-info-title">Nickname:</h3>
         <span className="profil-info">{user.nickname}</span>
-        <h3 className="profil-info">Adresse mail:</h3>
+        <h3 className="profil-info-title">Adresse mail:</h3>
         <span className="profil-info">{user.email}</span>
-        <h3 className="profil-info">Phone:</h3>
+        <h3 className="profil-info-title">Phone:</h3>
         <span className="profil-info">{user.phone}</span>
-        <h3 className="profil-info">City:</h3>
+        <h3 className="profil-info-title">City:</h3>
         <span className="profil-info">{user.city}</span>
-        <h3 className="profil-info">Birthdate:</h3>
+        <h3 className="profil-info-title">Birthdate:</h3>
         <span className="profil-info">{user.birthdate}</span>
-        <h3 className="profil-info">Gender:</h3>
+        <h3 className="profil-info-title">Gender:</h3>
         <span className="profil-info">{user.gender}</span>
-        <h3 className="profil-info">Bio:</h3>
+        <h3 className="profil-info-title">Bio:</h3>
         <span className="profil-info">{user.bio}</span>
       </div>
-      <div id="mes-animaux">
+      <div className="profil-section" id="mes-animaux">
         {/* Aller chercher dans la BDD les animaux liés à l'utilisateur
         et faire un map dessus */}
         <img className="profil-photo" src={user.dogPicture} alt="animal" />
-        <h3 className="profil-info">Name:</h3>
+        <h3 className="profil-info-title">Name:</h3>
         <span className="profil-info">Name</span>
-        <h3 className="profil-info">Breed:</h3>
+        <h3 className="profil-info-title">Breed:</h3>
         <span className="profil-info">{user.dog}</span>
-        <h3 className="profil-info">Sexe:</h3>
+        <h3 className="profil-info-title">Sexe:</h3>
         <span className="profil-info">Sexe</span>
-        <h3 className="profil-info">Personality:</h3>
+        <h3 className="profil-info-title">Personality:</h3>
         <span className="profil-info">Personality</span>
-        <h3 className="profil-info">Sterilized</h3>
+        <h3 className="profil-info-title">Sterilized</h3>
         <span className="profil-info">Sterilized</span>
       </div>
-      <div id="mes-messages">
+      <div className="profil-section" id="mes-messages">
         {/* Aller chercher dans la BDD les conversations et faire un map dessus, pour chaque
         conversation afficher le logo et le titre */}
         <img className="profil-message" src={conversation} alt="message" />
         <a href="" className="conversation">Conversation avec Nom de l'autre utilisation</a>
       </div>
-      <div id="mes-événements">
+      <div className="profil-section" id="mes-événements">
         {/* Aller chercher dans la BDD les événements liés à l'utilisateur
         et faire un map dessus */}
         <img className="profil-photo" src={event.picture} alt="evenement" />
-        <h3 className="profil-info">Name:</h3>
+        <h3 className="profil-info-title">Name:</h3>
         <span className="profil-info">{event.name}</span>
-        <h3 className="profil-info">Date:</h3>
+        <h3 className="profil-info-title">Date:</h3>
         <span className="profil-info">{event.date}</span>
-        <h3 className="profil-info">Description:</h3>
+        <h3 className="profil-info-title">Description:</h3>
         <span className="profil-info">{event.description}</span>
       </div>
 
