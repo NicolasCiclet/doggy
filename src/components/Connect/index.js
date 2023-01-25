93cd3768948b7617cfa34af6a73d12eb64f4a654
NicolasCiclet <ciclet.nicolas@free.fr>
2023-01-25 09:14:16 +0100

    modif on connexion form

--- src/components/Connect/index.js
+++ src/components/Connect/index.js
@@ -11,6 +11,8 @@ import { useSelector, useDispatch } from 'react-redux';
 import Field from 'src/components/Connect/Field';
 
 import { submitLogin, updateSettingsField } from '../../actions/user';
+import MainSection from '../MainSection';
+import Welcome from '../Welcome';
 
 import './connect.scss';
 
@@ -21,44 +23,48 @@ const Connect = () => {
   const dispatch = useDispatch();
 
   return (
-    <div className="settings">
-      <form
-        className="settings-form"
-        onSubmit={(event) => {
-          event.preventDefault();
-          dispatch(submitLogin());
-        }}
-      >
-        <Field
-          identifier="email"
-          placeholder="cestlamour@laplage.net"
-          label="E-mail"
-          value={emailValue}
-          changeField={(identifier, newValue) => {
-            // on constate que le paramètre identifier est rempli automatiquement avec
-            // la valeur qu'on a fournie pour la prop identifier.
-            // Et value contient la nouvelle valeur du champ, avec le nouveau caractère saisi
-            // console.log(`changeField sur l'e-mail : identifier=${identifier}
-            // newValue=${newValue}`);
+    <>
+      <div className="settings">
+        <h1 className="settings-title">Connexion</h1>
+        <form
+          className="settings-form"
+          onSubmit={(event) => {
+            event.preventDefault();
+            dispatch(submitLogin());
+          }}
+        >
+          <Field
+            identifier="email"
+            placeholder="cestlamour@laplage.net"
+            label="E-mail"
+            value={emailValue}
+            changeField={(identifier, newValue) => {
+              // on constate que le paramètre identifier est rempli automatiquement avec
+              // la valeur qu'on a fournie pour la prop identifier.
+              // Et value contient la nouvelle valeur du champ, avec le nouveau caractère saisi
+              // console.log(`changeField sur l'e-mail : identifier=${identifier}
+              // newValue=${newValue}`);
 
-            const action = updateSettingsField(identifier, newValue);
-            dispatch(action);
-          }}
-        />
-        <Field
-          identifier="password"
-          placeholder="mot de passe"
-          label="Mot de passe"
-          type="password"
-          value={passwordValue}
-          changeField={(identifier, newValue) => {
-            const action = updateSettingsField(identifier, newValue);
-            dispatch(action);
-          }}
-        />
-        <button type="submit" className="settings-submit">Connexion</button>
-      </form>
-    </div>
+              const action = updateSettingsField(identifier, newValue);
+              dispatch(action);
+            }}
+          />
+          <Field
+            identifier="password"
+            placeholder="mot de passe"
+            label="Mot de passe"
+            type="password"
+            value={passwordValue}
+            changeField={(identifier, newValue) => {
+              const action = updateSettingsField(identifier, newValue);
+              dispatch(action);
+            }}
+          />
+          <button type="submit" className="settings-submit">Login</button>
+        </form>
+      </div>
+      <MainSection />
+    </>
   );
 };
 
