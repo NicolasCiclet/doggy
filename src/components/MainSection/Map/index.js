b4fef1a61cf724bfbdbed33af23957810e608afc
NicolasCiclet <ciclet.nicolas@free.fr>
2023-01-27 15:49:17 +0100

    user page few modif

--- src/components/MainSection/Map/index.js
+++ src/components/MainSection/Map/index.js
@@ -32,6 +32,7 @@ const Map = () => {
   const events = useSelector((state) => state.event.eventsToDisplay);
   const users = useSelector((state) => state.user.usersToDisplay);
   const markerSelected = useSelector((state) => state.map.nameSelected);
+  const isLogged = useSelector((state) => state.user.logged);
   return (
     <div className="map-main">
       {/* default position of the map */}
@@ -42,44 +43,45 @@ const Map = () => {
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
         {/* display a button to show or hide the markers */}
-        <LayersControl position="topright">
+        {isLogged && (
+          <LayersControl position="topright">
 
-          <LayersControl.Overlay checked name="Events">
-            <FeatureGroup>
-              {/* I map on events to display all markers */}
-              {events.map((event) => (
-                <Marker
-                  position={[event.lat, event.lng]}
-                  icon={(event.name === markerSelected) ? icon4 : icon1}
-                  key={event.id}
-                >
-                  <Popup>
-                    {event.name} <br /> {event.difficulty}
-                  </Popup>
-                </Marker>
-              ))}
-            </FeatureGroup>
-          </LayersControl.Overlay>
+            <LayersControl.Overlay checked name="Events">
+              <FeatureGroup>
+                {/* I map on events to display all markers */}
+                {events.map((event) => (
+                  <Marker
+                    position={[event.lat, event.lng]}
+                    icon={(event.name === markerSelected) ? icon4 : icon1}
+                    key={event.id}
+                  >
+                    <Popup>
+                      {event.name} <br /> {event.difficulty}
+                    </Popup>
+                  </Marker>
+                ))}
+              </FeatureGroup>
+            </LayersControl.Overlay>
 
-          <LayersControl.Overlay checked name="Users">
-            <FeatureGroup>
-              {users.map((user) => (
-                <Marker
-                // Use a ternary condition to display different icons if the name matches
-                  position={[user.lat, user.lng]}
-                  icon={(user.firstname === markerSelected) ? icon3 : icon2}
-                  key={user.id}
-                >
-                  <Popup>
-                    {user.firstname} <br /> {user.gender}
-                  </Popup>
-                </Marker>
-              ))}
-            </FeatureGroup>
-          </LayersControl.Overlay>
+            <LayersControl.Overlay checked name="Users">
+              <FeatureGroup>
+                {users.map((user) => (
+                  <Marker
+                  // Use a ternary condition to display different icons if the name matches
+                    position={[user.lat, user.lng]}
+                    icon={(user.firstname === markerSelected) ? icon3 : icon2}
+                    key={user.id}
+                  >
+                    <Popup>
+                      {user.firstname} <br /> {user.gender}
+                    </Popup>
+                  </Marker>
+                ))}
+              </FeatureGroup>
+            </LayersControl.Overlay>
 
-        </LayersControl>
-
+          </LayersControl>
+        )}
       </MapContainer>
     </div>
   );
--- src/components/MainSection/Users/userPage.js
+++ src/components/MainSection/Users/userPage.js
@@ -1,5 +1,4 @@
 import { useDispatch, useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
 import { isMessFormOpened } from '../../../actions/user';
 
@@ -13,12 +12,16 @@ const UserPage = () => {
   const { id } = useParams();
   const user = useSelector((state) => findUser(state.user.usersToDisplay, id));
   const isFormOpen = useSelector((state) => state.user.messFormOpen);
+
+  //TODO a modifier avec l'apel a l'api back
+  const events = useSelector((state) => state.event.eventsToDisplay);
+  const event = events.find((onEvent) => (onEvent.id === 1));
   const dispatch = useDispatch();
   // console.log(user);
   // console.log(useSelector((state) => state.user.usersToDisplay));
   return (
     <div className="userboard">
-      <div className="userboard-section">
+      <div className="userboard-leftsection">
         {/* Afficher les infos du user connecté */}
         <div className="userboard-header">
           <h1 className="userboard-h1">Moi</h1>
@@ -51,55 +54,84 @@ const UserPage = () => {
           </div>
         </div>
       </div>
-      <div className="userboard-section userboard-section-desktop" id="mes-animaux">
-        {/* Aller chercher dans la BDD les animaux liés à l'utilisateur
-        et faire un map dessus */}
-        <div className="userboard-header">
-          <h1 className="userboard-h1">Mes animaux</h1>
-        </div>
-        <div className="userboard-main">
-          <div className="userboard-main-photo">
-            <img className="userboard-photo" src={user.dogPicture} alt="animal" />
+      <section className="userboard-rightsection-desktop">
+        <div className="userboard-rightsection" id="mes-animaux">
+          {/* Aller chercher dans la BDD les animaux liés à l'utilisateur
+          et faire un map dessus */}
+          <div className="userboard-header">
+            <h1 className="userboard-h1">Mes animaux</h1>
           </div>
-          <div className="userboard-main-infos">
+          <div className="userboard-main">
+            <div className="userboard-main-photo">
+              <img className="userboard-photo" src={user.dogPicture} alt="animal" />
+            </div>
+            <div className="userboard-main-infos">
 
-            <h3 className="userboard-info">Name</h3>
+              <h3 className="userboard-info">Name</h3>
 
-            <h3 className="userboard-info">{user.dog}</h3>
+              <h3 className="userboard-info">{user.dog}</h3>
 
-            <h3 className="userboard-info">Sexe</h3>
+              <h3 className="userboard-info">Sexe</h3>
 
-            <h3 className="userboard-info">Personality</h3>
+              <h3 className="userboard-info">Personality</h3>
 
-            <h3 className="userboard-info">Sterilized</h3>
+              <h3 className="userboard-info">Sterilized</h3>
 
-            <blockquote className="userboard-bio">Je suis très sociable, j'adore les autre chiens et même les chats. Je déborde d'énergie pour de longues balades.</blockquote>
+              <blockquote className="userboard-bio">Je suis très sociable, j'adore les autre chiens et même les chats. Je déborde d'énergie pour de longues balades.</blockquote>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="userboard-section" id="mes-événements">
-        {/* Aller chercher dans la BDD les événements liés à l'utilisateur
-        et faire un map dessus */}
-        <div className="userboard-header">
-          <h1 className="userboard-h1">Mes événements</h1>
-        </div>
-        {/* <div className="userboard-main">
-          <div className="userboard-main-photo">
-            <img className="userboard-photo" src={event.picture} alt="evenement" />
+        <div className="userboard-rightsection" id="mes-animaux">
+          {/* Aller chercher dans la BDD les animaux liés à l'utilisateur
+          et faire un map dessus */}
+          <div className="userboard-header">
+            <h1 className="userboard-h1">Mes animaux</h1>
           </div>
-          <div className="userboard-main-infos">
+          <div className="userboard-main">
+            <div className="userboard-main-photo">
+              <img className="userboard-photo" src={user.dogPicture} alt="animal" />
+            </div>
+            <div className="userboard-main-infos">
+
+              <h3 className="userboard-info">Name</h3>
+
+              <h3 className="userboard-info">{user.dog}</h3>
+
+              <h3 className="userboard-info">Sexe</h3>
+
+              <h3 className="userboard-info">Personality</h3>
+
+              <h3 className="userboard-info">Sterilized</h3>
+
+              <blockquote className="userboard-bio">Je suis très sociable, j'adore les autre chiens et même les chats. Je déborde d'énergie pour de longues balades.</blockquote>
+            </div>
+          </div>
+        </div>
+        <div className="userboard-rightsection" id="mes-événements">
+          {/* Aller chercher dans la BDD les événements liés à l'utilisateur
+          et faire un map dessus */}
+          <div className="userboard-header">
+            <h1 className="userboard-h1">Mes événements</h1>
+          </div>
+          <div className="userboard-main">
+            <div className="userboard-main-photo">
+              <img className="userboard-photo" src={event.picture} alt="evenement" />
+            </div>
+            <div className="userboard-main-infos">
 
               <h3 className="userboard-info-title">Name:</h3>
               <span className="userboard-info">{event.name}</span>
 
               <h3 className="userboard-info-title">Date:</h3>
               <span className="userboard-info">{event.date}</span>
-          </div>
-        </div>
-        <h3 className="userboard-info-title">Description:</h3>
-        <span className="userboard-info">{event.description}</span> */}
 
-      </div>
+            </div>
+          </div>
+          <h3 className="userboard-info-title">Description:</h3>
+          <span className="userboard-info">{event.description}</span>
+
+        </div>
+      </section>
     </div>
   );
 };
