e24240afd38213ce20cb054bc01dc60149f80281
NicolasCiclet <ciclet.nicolas@free.fr>
2023-02-01 12:05:27 +0100

    Wrong branch sorry, Send new event ok

--- src/components/Register/newEvent.js
+++ src/components/Register/newEvent.js
@@ -1,9 +1,10 @@
 import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import {
-  Button, Form, Select, TextArea,
+  Button, Form, Message, Select, TextArea,
 } from 'semantic-ui-react';
 import {
-  dateNewEvent, describNewEvent, placeNewEvent, submitFormNewEvent, titleNewEvent,
+  dateNewEvent, describNewEvent, newEventCreated, placeNewEvent, submitFormNewEvent, titleNewEvent,
 } from '../../actions/event';
 
 import './register.scss';
@@ -15,64 +16,82 @@ const NewEvent = () => {
   const placeOptions = allPlace.map((place) => (
     { text: place.name, value: place.id }
   ));
-
+  const eventCreate = useSelector((state) => state.event.newEventCreated);
+  const eventName = useSelector((state) => state.event.titleNewEvent);
+  const navigate = useNavigate();
   const dispatch = useDispatch();
   return (
-    <div className="register">
-      <h1>Ajouter un évènement</h1>
-      <Form onSubmit={(event) => {
-        event.preventDefault();
-        console.log('new evenement submit');
-        dispatch(submitFormNewEvent());
-      }}
-      >
-        {/* Input for title */}
-        <Form.Input
-          label="Titre"
-          placeholder="Titre de l'évènement"
-          onChange={(event) => {
-            dispatch(titleNewEvent(event.target.value));
-          }}
-        />
-        {/* Inputs for time */}
-        <Form.Input
-          label="Date"
-          placeholder="Date"
-          type="datetime-local"
-          onChange={(event) => {
-            dispatch(dateNewEvent(event.target.value));
-          }}
-        />
-        {/* Input for place */}
-        <Form.Input
-          control={Select}
-          options={placeOptions}
-          label="Lieu"
-          placeholder="Lieu"
-          onChange={(event, result) => {
-            // console.log(`change : ${event.target.value}`);
-            dispatch(placeNewEvent(result.value));
-          }}
-        />
+    <>
+      {/* success message when eventCreate is true */}
+      {eventCreate && (
+        <Form success className="register-success">
+          <Message
+            success
+            header="Evènement ajouté avec succès"
+            content={`Nouvel évènement ${eventName}`}
+            onDismiss={() => {
+              dispatch(newEventCreated());
+              navigate('/profile');
+            }}
+          />
+        </Form>
+      )}
+      <div className="register">
+        <h1>Ajouter un évènement</h1>
+        <Form onSubmit={(event) => {
+          event.preventDefault();
+          console.log('new evenement submit');
+          dispatch(submitFormNewEvent());
+        }}
+        >
+          {/* Input for title */}
+          <Form.Input
+            label="Titre"
+            placeholder="Titre de l'évènement"
+            onChange={(event) => {
+              dispatch(titleNewEvent(event.target.value));
+            }}
+          />
+          {/* Inputs for time */}
+          <Form.Input
+            label="Date"
+            placeholder="Date"
+            type="datetime-local"
+            onChange={(event) => {
+              dispatch(dateNewEvent(event.target.value));
+            }}
+          />
+          {/* Input for place */}
+          <Form.Input
+            control={Select}
+            options={placeOptions}
+            label="Lieu"
+            placeholder="Lieu"
+            onChange={(event, result) => {
+              // console.log(`change : ${event.target.value}`);
+              dispatch(placeNewEvent(result.value));
+            }}
+          />
 
-        {/* Input for description */}
-        <Form.Input
-          className="add-new-event-description"
-          label="Description de l'évènement"
-          control={TextArea}
-          placeholder="Description de l'évènement"
-          onChange={(event) => {
-            dispatch(describNewEvent(event.target.value));
-          }}
-        />
-        {/* Input for submit */}
-        <Form.Input
-          control={Button}
-          content="Valider"
-        />
-      </Form>
+          {/* Input for description */}
+          <Form.Input
+            className="add-new-event-description"
+            label="Description de l'évènement"
+            control={TextArea}
+            placeholder="Description de l'évènement"
+            onChange={(event) => {
+              dispatch(describNewEvent(event.target.value));
+            }}
+          />
+          {/* Input for submit */}
+          <Form.Input
+            control={Button}
+            content="Valider"
+          />
+        </Form>
 
-    </div>
+      </div>
+    </>
   );
 };
 
