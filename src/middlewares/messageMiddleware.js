1aa94017fa13a4ffdaf1b37315cf0108b41016f8
NicolasCiclet <ciclet.nicolas@free.fr>
2023-02-06 22:56:54 +0100

    few modif on messages pages

--- src/middlewares/messageMiddleware.js
+++ src/middlewares/messageMiddleware.js
@@ -1,13 +1,12 @@
 import axios from 'axios';
-import { GET_ALL_CONVERSATIONS, GET_ONE_CONVERSATION, isMessageSend, POST_USER_MESSAGE, stockAllConversations, stockOneConversation,
-CONTACT_SEND_MAIL, isMessageSend, POST_USER_MESSAGE, contactFormReset,
+import {
+  GET_ALL_CONVERSATIONS, isMessageSend, POST_USER_MESSAGE, stockAllConversations,
+  CONTACT_SEND_MAIL, contactFormReset,
 } from '../actions/message';
-
 
 const messageMiddleware = (store) => (next) => (action) => {
   // eslint-disable-next-line prefer-destructuring
   const url = store.getState().nav.url;
-  const idConversation = store.getState().message.idConversationApi;
 
   switch (action.type) {
     case POST_USER_MESSAGE:
@@ -63,38 +62,6 @@ const messageMiddleware = (store) => (next) => (action) => {
           console.log(response.data.conversations);
           console.log('conversations recu');
           store.dispatch(stockAllConversations(response.data.conversations));
-        })
-        // What to do in case of error
-        .catch((error) => {
-          console.log(error);
-          console.log('conversation non trouvé');
-        })
-
-        // to do in any case
-        .finally(() => {
-        });
-
-      break;
-
-    // case GET_ONE_CONVERSATION:
-      console.log('reception d\'une seule conversation du back');
-
-      // I send the request
-      axios.get(
-        `${url}api/conversations/${idConversation}/messages/`,
-        {
-          headers: {
-            Authorization: `Bearer ${store.getState().user.token}`,
-          },
-        },
-      )
-      // Wait for the response
-        .then((response) => {
-          console.log(response);
-          console.log('1 conversation recu');
-          // const { description } = response.data.conversation;
-          // const { content, title, id } = response.data.messages;
-          store.dispatch(stockOneConversation(response.data.messages));
         })
         // What to do in case of error
         .catch((error) => {
