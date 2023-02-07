0e010e614d61118a3346190beb0c82ae3533d82e
NicolasCiclet <ciclet.nicolas@free.fr>
2023-02-07 22:31:35 +0100

    few design modif

--- src/components/MainSection/Loader/index.js
+++ src/components/MainSection/Loader/index.js
@@ -1,9 +1,10 @@
 import dogWalking from 'src/data/dog_walking.gif';
+import dogWalking2 from 'src/data/dog_walking2.gif';
 import './loader.scss';
 
 const Loader = () => (
   <div className="loader">
-    <img src={dogWalking} alt="" />
+    <img src={dogWalking2} alt="" />
     <p className="loader-info">
       Veuillez patienter svp...
     </p>
