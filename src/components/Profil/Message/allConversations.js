d863ad2287f7040a70fdf2c0c67ac6a1bdc5ce74
NicolasCiclet <ciclet.nicolas@free.fr>
2023-03-14 17:31:28 +0100

    cleaning message code

--- src/components/Profil/Message/allConversations.js
+++ src/components/Profil/Message/allConversations.js
@@ -1,4 +1,5 @@
 import { useDispatch, useSelector } from 'react-redux';
+import { useState } from 'react';
 import { activeConversation, getAllConversations, patchreadMessages } from '../../../actions/message';
 import logoMail from '../conversation.svg';
 import OneConversation from './oneConversation';
@@ -6,12 +7,24 @@ import OneConversation from './oneConversation';
 import './conversation.scss';
 import { updateUnreadMessage } from '../../../actions/user';
 
+// Component with all conversations
 const AllConversations = () => {
   const dispatch = useDispatch();
-  const showConversation = useSelector((state) => state.message.activeConversation);
   const allConversations = useSelector((state) => state.message.allConversationsApi);
   const unreadMessages = useSelector((state) => state.message.unreadMessages);
-  // const userNickname = useSelector((state) => state.user.usernameNewUser);
+
+  // each element of the new array will be initialized to false
+  const [isOpen, setIsOpen] = useState(allConversations.map(() => false));
+  const handleClick = (index) => {
+    /* Create a copy of isOpen with spread operator
+    To respect the principle of immutability, which consists in not modifying the data directly,
+    but in creating a new modified copy from the original */
+    const newOpenState = [...isOpen];
+    // on click we pass the index as argument and we invert the value
+    newOpenState[index] = !isOpen[index];
+    // we update the state of isOpen
+    setIsOpen(newOpenState);
+  };
 
   return (
     <div className="profil-section" id="mes-messages">
@@ -19,36 +32,40 @@ const AllConversations = () => {
         <h1 className="profil-h1">Mes messages</h1>
       </div>
       <div className="profil-main-message">
-        {allConversations.map((conversation) => (
+        {allConversations.map((conversation, index) => (
           <>
             <p
               onClick={() => {
-                // dispatch(showOneConversation());
+                // sends to state the conversation's id
                 dispatch(activeConversation(conversation.id));
-                console.log(conversation);
+                // tell to the back that the messages are read with the conversation's id
                 dispatch(patchreadMessages());
+                // gets the number of unread messages updated per conversation
                 dispatch(getAllConversations());
+                // gets the number of unread messages updated for all conversations
                 dispatch(updateUnreadMessage());
+                // we call the function by passing it the index
+                handleClick(index);
               }}
-              className={conversation.id == showConversation ? 'conversation-open' : 'conversation'}
+              // we use a ternary condition
+              className={isOpen[index] ? 'conversation-open' : 'conversation'}
               key={conversation.id}
             >
               <img className="logo-mail" src={logoMail} alt="message" />
+              {/* The name of the 2 persons */}
               {conversation.users[1].nickname} & {conversation.users[0].nickname}
-              {/* {console.log(unreadMessages[conversation.id])} */}
+              {/* If the number of unread messages > 0, the pastille is displayed */}
               {unreadMessages[conversation.id].nbrUnreadMessage > 0 && (
               <div className="unreadMessage">{unreadMessages[conversation.id].nbrUnreadMessage}</div>
               )}
 
             </p>
-            {(showConversation == conversation.id)
+            {/* if isOpen[index] is true, the conversation is displayed */}
+            {isOpen[index]
+            // I use a spread operator to distribute all the properties
             && <OneConversation {...conversation} messages={conversation.messages} />}
           </>
         ))}
-
-        {/* {openConversation && (
-          <OneConversation />
-        )} */}
       </div>
     </div>
   );
--- src/components/Profil/Message/oneConversation.js
+++ src/components/Profil/Message/oneConversation.js
@@ -1,20 +1,18 @@
 import moment from 'moment';
 import PropTypes from 'prop-types';
 
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 
-const OneConversation = ({ id, messages }) => {
-  console.log(messages);
-  const dispatch = useDispatch();
+const OneConversation = ({ messages }) => {
   const idUser = useSelector((state) => state.user.idNewUser);
   return (
     <div className="one-conversation">
       {messages.map((message) => (
         <div className="one-message">
+          {/* allows to classify messages as received or sent */}
           <p className={message.author.id == idUser ? 'sent' : 'received'}>
             {message.content} <span className="message-date">{moment(message.createdAt).locale('fr').format('LLLL')}</span>
           </p>
-          {/* <p className="message-date">{moment(message.createdAt).locale('fr').format('LLLL')}</p> */}
         </div>
       ))}
     </div>
@@ -22,8 +20,7 @@ const OneConversation = ({ id, messages }) => {
 };
 
 OneConversation.propTypes = {
-  /** identifier for the input : used both for name and id => must be unique */
-  id: PropTypes.number.isRequired,
+  messages: PropTypes.string.isRequired,
 };
 
 export default OneConversation;
